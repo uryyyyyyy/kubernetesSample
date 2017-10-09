2d3c9c26c25431be7dc134e00570bbe87ae98996
uryyyyyyy <koki305@gmail.com>
2017-10-09 21:27:08 +0900

    add stg

---
+++ apps/spring-app/src/main/kotlin/com/github/uryyyyyyy/springbootkotlin/controller/IndexController.kt
@@ -0,0 +1,22 @@
+package com.github.uryyyyyyy.springbootkotlin.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class IndexController {
+
+  @GetMapping("/")
+  fun hello(): String {
+    return "index"
+  }
+
+  @GetMapping("/healthz")
+  fun welcome(): String {
+    return "healthz"
+  }
+
+}
--- kubernetes/README-gke.md
+++ kubernetes/README-gke.md
@@ -10,3 +10,11 @@ TBD
 ```
 gcloud container clusters get-credentials cluster-1 --zone asia-northeast1-a --project ${PROJECT_ID}
 ```
+
+## ingress
+
+static IPに紐付ける
+
+```
+gcloud compute addresses create test-ip --global
+```
